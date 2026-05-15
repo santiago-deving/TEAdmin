412256ad36e642c3a6f53c5927cf2ac078936833
santiago-deving <vinicius.sbio.so@gmail.com>
2026-05-14 23:46:20 -0300

    debug 6 - agora vai?

--- middlewares/dataFunctions.js
+++ middlewares/dataFunctions.js
@@ -4,49 +4,50 @@ async function calcFreq(id_paciente, id_profissional, req) {
     const usuario = req.session.usuario;
     const client = await db.connect();
 
-    let result;
+    try {
+        let result;
 
-    if (usuario.tipo === 0 && id_profissional == null) {
+        if (usuario.tipo === 0 && id_profissional == null) {
 
-        result = await client.query(
-            'SELECT calcfreq($1::integer)',
-            [id_paciente]
-        );
+            result = await client.query(
+                'SELECT calcfreq($1::integer)',
+                [id_paciente]
+            );
 
-    } else if (usuario.tipo === 0 && id_profissional != null) {
+        } else if (usuario.tipo === 0 && id_profissional != null) {
 
-        result = await client.query(
-            'SELECT calcfreq($1::integer, $2::integer)',
-            [id_paciente, id_profissional]
-        );
+            result = await client.query(
+                'SELECT calcfreq($1::integer, $2::integer)',
+                [id_paciente, id_profissional]
+            );
 
-    } else if (usuario.tipo === 1) {
+        } else if (usuario.tipo === 1) {
 
-        result = await client.query(
-            'SELECT calcfreq($1::integer, $2::integer)',
-            [id_paciente, usuario.id_profissional]
-        );
+            result = await client.query(
+                'SELECT calcfreq($1::integer, $2::integer)',
+                [id_paciente, usuario.id_profissional]
+            );
 
-    } else if (usuario.tipo === 2 && id_profissional == null) {
+        } else if (usuario.tipo === 2 && id_profissional == null) {
 
-        result = await client.query(
-            'SELECT calcfreq($1::integer)',
-            [id_paciente]
-        );
+            result = await client.query(
+                'SELECT calcfreq($1::integer)',
+                [id_paciente]
+            );
 
-    } else if (usuario.tipo === 2 && id_profissional != null) {
+        } else if (usuario.tipo === 2 && id_profissional != null) {
 
-        result = await client.query(
-            'SELECT calcfreq($1::integer, $2::integer)',
-            [id_paciente, id_profissional]
-        );
+            result = await client.query(
+                'SELECT calcfreq($1::integer, $2::integer)',
+                [id_paciente, id_profissional]
+            );
+        }
+
+        return result.rows[0].calcfreq;
+
+    } finally {
+        client.release();
     }
-
-    const freq = result.rows[0].calcfreq;
-
-    client.release();
-
-    return freq;
 }
 
 module.exports = {calcFreq}
